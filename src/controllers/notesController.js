47a98a133d91ef9f4c47cbc8a097754398f41382
d-yv <yuri.dubrovin@gmail.com>
2025-10-02 22:38:12 +0300

    add hw2

---
+++ src/controllers/notesController.js
@@ -0,0 +1,84 @@
+import createHttpError from 'http-errors';
+import { Note } from '../models/notes.js';
+import mongoose from 'mongoose';
+
+export const getNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(noteId)) {
+      return next(createHttpError(400, 'Invalid note id'));
+    }
+
+    const note = await Note.findOne({ _id: noteId });
+
+    if (!note) {
+      return next(createHttpError(404, 'Note not found'));
+    }
+
+    res.status(200).json(note);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(noteId)) {
+      return next(createHttpError(400, 'Invalid note id'));
+    }
+
+    const note = await Note.findOneAndDelete({ _id: noteId });
+
+    if (!note) {
+      return next(createHttpError(404, 'Note not found'));
+    }
+
+    res.status(200).json(note);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(noteId)) {
+      return next(createHttpError(400, 'Invalid note id'));
+    }
+
+    const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!note) {
+      return next(createHttpError(404, 'Note not found'));
+    }
+
+    res.status(200).json(note);
+  } catch (err) {
+    next(err);
+  }
+};
