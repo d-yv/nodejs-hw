cd11f3916fa735f3c956f270d2db5f625eefebb9
d-yv <yuri.dubrovin@gmail.com>
2025-10-02 23:45:04 +0300

    fix filename note.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { Note } from '../models/notes.js';
+import { Note } from '../models/note.js';
 import mongoose from 'mongoose';
 
 export const getNotes = async (req, res, next) => {
