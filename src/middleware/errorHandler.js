ca8ba500c440c87b014a024b7efd78030888eebd
d-yv <yuri.dubrovin@gmail.com>
2025-10-02 23:04:16 +0300

    change ErrorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,13 +1,17 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
+  console.log('Error Middleware: ', err.message);
+
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
     });
   }
-  console.error(err.message);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: err.message,
+    message: isProd ? 'Something broken' : err.message,
   });
 };
