67c53b8ebd0c04b91bfbbba9a2b86469adeab3cb
d-yv <yuri.dubrovin@gmail.com>
2025-10-21 20:11:28 +0300

    add avatar

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 3030;
 const app = express();
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
